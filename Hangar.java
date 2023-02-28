a696c0e45d5bca79173479cff3823e4d73174df0
CamiDINET <117636454+CamiDINET@users.noreply.github.com>
2023-02-28 15:23:47 +0100

    Create Hangar.java

---
+++ Hangar.java
@@ -0,0 +1,10 @@
+public class Hangar {
+    public static void main(String[] args) {
+
+
+        Car renault = new Car(10000, "Megane");
+        renault.doStuff();
+        Boat titanic = new Boat(500000, "Titanic"); 
+        titanic.doStuff();
+    }
+}
